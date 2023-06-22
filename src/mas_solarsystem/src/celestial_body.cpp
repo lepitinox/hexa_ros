8669f5d02642292bd73be0ce9dab326a5c04067c
Adrien <adrienducourthial@gmail.com>
2023-06-22 12:04:43 +0200

    azeaze

---
+++ src/mas_solarsystem/src/celestial_body.cpp
@@ -0,0 +1,10 @@
+// class that represents a celestial body
+
+class celestial_body
+{
+private:
+    /* data */
+public:
+    celestial_body(/* args */);
+    ~celestial_body();
+};
---
+++ src/mas_solarsystem/src/celestial_body.h
@@ -0,0 +1,5 @@
+// h file of CelestialBody class
+
+#ifndef CELESTIAL_BODY_H
+#define CELESTIAL_BODY_H
+
