9ba7072ea95d41be13d913d2f9cb95f1c9a820dd
Adrien <adrienducourthial@gmail.com>
2023-06-23 15:29:13 +0200

    sqd

--- src/mas_solarsystem/src/celestial_body_node.cpp
+++ src/mas_solarsystem/src/celestial_body_node.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include "rclcpp/rclcpp.hpp"
 #include "celestial_body.h"
+#include <string>
 
 
 
 int main(int argc, char *argv[]) {
     rclcpp::init(argc, argv);
+    std:string name = argv[1];
+    double mass = std::stod(argv[2]);
+    double orbit_radius = std::stod(argv[3]);
 
-    auto planet = std::make_shared<CelestialBody>(argv[1], argv[7],argv[2]);
+    auto planet = std::make_shared<CelestialBody>(name, mass, orbit_radius);
 
     rclcpp::spin(planet);  // Keep the script running with earth as the main node
 
