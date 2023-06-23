4106be0f511328113d784696cbe0f7dd4b735b67
Adrien <adrienducourthial@gmail.com>
2023-06-23 17:31:10 +0200

    qsd

--- src/mas_solarsystem/src/celestial_body.cpp
+++ src/mas_solarsystem/src/celestial_body.cpp
@@ -47,29 +47,29 @@ void CelestialBody::update()
         t.transform.translation.z = 0.0;
         tf_broadcaster_->sendTransform(t);
     
-    // Update the marker.
-    visualization_msgs::msg::Marker marker;
-    marker.header.stamp = this->get_clock()->now();
-    marker.header.frame_id = name;
-    marker.ns = "";
-    marker.id = id;
-    marker.type = visualization_msgs::msg::Marker::SPHERE;
-    marker.action = visualization_msgs::msg::Marker::ADD;
-    marker.pose.position.x = 1;
-    marker.pose.position.y = 1;
-    marker.pose.position.z = 1;
-    marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = 1.0;
-    marker.scale.x = 1;
-    marker.scale.y = 0.1;
-    marker.scale.z = 0.1;
-    marker.color.a = 1.0; // Don't forget to set the alpha!
-    marker.color.r = 0.0;
-    marker.color.g = 1.0;
-    marker.color.b = 0.0;
-    marker_pub_->publish(marker);
+        // Update the marker.
+        visualization_msgs::msg::Marker marker;
+        marker.header.stamp = this->get_clock()->now();
+        marker.header.frame_id = name;
+        marker.ns = "";
+        marker.id = id;
+        marker.type = visualization_msgs::msg::Marker::SPHERE;
+        marker.action = visualization_msgs::msg::Marker::ADD;
+        marker.pose.position.x = 1;
+        marker.pose.position.y = 1;
+        marker.pose.position.z = 1;
+        marker.pose.orientation.x = 0.0;
+        marker.pose.orientation.y = 0.0;
+        marker.pose.orientation.z = 0.0;
+        marker.pose.orientation.w = 1.0;
+        marker.scale.x = 1;
+        marker.scale.y = 10;
+        marker.scale.z = 10;
+        marker.color.a = 1.0; // Don't forget to set the alpha!
+        marker.color.r = 1.0;
+        marker.color.g = 1.0;
+        marker.color.b = 1.0;
+        marker_pub_->publish(marker);
 //    marker_.frame_locked = true;
     }else{
 
@@ -101,18 +101,18 @@ void CelestialBody::update()
     marker.id = id;
     marker.type = visualization_msgs::msg::Marker::SPHERE;
     marker.action = visualization_msgs::msg::Marker::ADD;
-    marker.pose.position.x = x;
-    marker.pose.position.y = y;
-    marker.pose.position.z = 1;
+    marker.pose.position.x = 0.0;
+    marker.pose.position.y = 0.0;
+    marker.pose.position.z = 0.0;
     marker.pose.orientation.x = 0.0;
     marker.pose.orientation.y = 0.0;
     marker.pose.orientation.z = 0.0;
     marker.pose.orientation.w = 1.0;
-    marker.scale.x = 1;
-    marker.scale.y = 0.1;
-    marker.scale.z = 0.1;
+    marker.scale.x = 10;
+    marker.scale.y = 10;
+    marker.scale.z = 10;
     marker.color.a = 1.0; // Don't forget to set the alpha!
-    marker.color.r = 0.0;
+    marker.color.r = 1.0;
     marker.color.g = 1.0;
     marker.color.b = 0.0;
     marker_pub_->publish(marker);
