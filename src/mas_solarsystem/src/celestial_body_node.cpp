45dede80a375b5b0e56a76ddfccabc169d21c4da
Adrien <adrienducourthial@gmail.com>
2023-06-23 14:54:32 +0200

    aazeaze

--- src/mas_solarsystem/src/celestial_body_node.cpp
+++ src/mas_solarsystem/src/celestial_body_node.cpp
@@ -15,13 +15,14 @@ int main(int argc, char *argv[]) {
     for (int i = 1; i < argc; i++) {
       ss << argv[i] << ' ';
     }
+    RCLCPP_INFO(logger, "celestial_body_node started with args: %s", ss.str().c_str());
         
     // use parameters from launch file
+    auto planet = std::make_shared<CelestialBody>("planet", 5.972e24, Vector3d(0, 147e9, 0), Vector3d(30e3, 0, 0));
 
 //    auto earth = std::make_shared<CelestialBody>("Earth", 5.972e24, Vector3d(0, 147e9, 0), Vector3d(30e3, 0, 0));
 //    auto moon = std::make_shared<CelestialBody>("Moon", 7.342e22, Vector3d(385e6, 147e9, 0), Vector3d(1e3, 30e3, 0));
-
-//    rclcpp::spin(earth);  // Keep the script running with earth as the main node
+    rclcpp::spin(planet);  // Keep the script running with earth as the main node
 
     // Destroy the nodes when the script is stopped
     rclcpp::shutdown();
