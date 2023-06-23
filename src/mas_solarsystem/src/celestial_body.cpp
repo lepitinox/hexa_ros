0c6f8ebffa5e2c814ee2a3f2ef15d73129d2b6d4
Adrien <adrienducourthial@gmail.com>
2023-06-23 16:41:01 +0200

    azeasd

--- src/mas_solarsystem/src/celestial_body.cpp
+++ src/mas_solarsystem/src/celestial_body.cpp
@@ -11,14 +11,12 @@
 #include "visualization_msgs/msg/marker.hpp"
 
 
-CelestialBody::CelestialBody(
-  const std::string &name,
-  double mass,
-  double orbit_radius) : Node(name),
-    name_(name),
-    mass_(mass),
-    orbit_radius_(orbit_radius){
-
+CelestialBody::CelestialBody : Node("celestial_body_node",
+            rclcpp::NodeOptions()
+                .allow_undeclared_parameters(true)
+                .automatically_declare_parameters_from_overrides(true))
+{
+    
 
     auto update_time = std::chrono::milliseconds(10);
     auto timer_ = create_wall_timer(update_time, std::bind(&CelestialBody::update, this));
@@ -31,13 +29,15 @@ double CelestialBody::calculate_omega(double G, double M, double r) {
 }
 
 void CelestialBody::update()
-{   
+{   auto name = this->get_parameter("name").as_string();
+    auto mass = this->get_parameter("Masse").as_double();
+    auto orbit_radius = this->get_parameter("Orbite").as_double();
 
-    if (name_ == "Soleil"){
+    if (name == "Soleil"){
         geometry_msgs::msg::TransformStamped t;
         t.header.stamp = this->get_clock()->now();
         t.header.frame_id = "world";
-        t.child_frame_id = get_name();
+        t.child_frame_id = name;
         t.transform.translation.x = 0.0;
         t.transform.translation.y = 0.0;
         t.transform.translation.z = 0.0;
@@ -53,12 +53,12 @@ void CelestialBody::update()
     }else{
 
     auto g_constant_ = 6.67430e-11;
-    double angular_velocity_ = calculate_omega(g_constant_, mass_, orbit_radius_);
+    double angular_velocity_ = calculate_omega(g_constant_, mass, orbit_radius);
 
     double angle = angular_velocity_ * this->get_clock()->now().seconds();
 
-    double x = orbit_radius_ * cos(angle);
-    double y = orbit_radius_ * sin(angle);
+    double x = orbit_radius * cos(angle);
+    double y = orbit_radius * sin(angle);
 
     geometry_msgs::msg::TransformStamped t;
     t.header.stamp = this->get_clock()->now();
