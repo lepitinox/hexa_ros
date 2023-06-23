3e99a35601fccdb277f23688e3a037a37342ce38
Adrien <adrienducourthial@gmail.com>
2023-06-23 14:06:57 +0200

    sais plus faire de cpp

--- src/mas_solarsystem/src/celestial_body.cpp
+++ src/mas_solarsystem/src/celestial_body.cpp
@@ -11,7 +11,7 @@
 #include "visualization_msgs/msg/marker.hpp"
 
 
-class CelestialBody::CelestialBody(
+CelestialBody::CelestialBody(
   const std::string &name,
   double mass,
   std::vector<double> position,
@@ -32,40 +32,40 @@ class CelestialBody::CelestialBody(
     ros::Publisher vis_pub = node_handle.advertise<visualization_msgs::msg::Marker>( "visualization_marker", 0 );
 
 }
-    double CelestialBody::calculate_omega(double G, double M, double r) {
-        double omega = std::sqrt(G * M / std::pow(r, 3));
-        return omega;
-    }
+double CelestialBody::calculate_omega(double G, double M, double r) {
+    double omega = std::sqrt(G * M / std::pow(r, 3));
+    return omega;
+}
 
-    void CelestialBody::update()
-    {
-        auto g_constant_ = 6.67430e-11;
-        double angular_velocity_ = calculate_omega(g_constant_, mass_, orbit_radius_);
-        double angle = timer_ * angular_velocity_;
+void CelestialBody::update()
+{
+    auto g_constant_ = 6.67430e-11;
+    double angular_velocity_ = calculate_omega(g_constant_, mass_, orbit_radius_);
+    double angle = timer_ * angular_velocity_;
 
-        double x = orbit_radius_ * cos(angle);
-        double y = orbit_radius_ * sin(angle);
+    double x = orbit_radius_ * cos(angle);
+    double y = orbit_radius_ * sin(angle);
 
-        geometry_msgs::msg::TransformStamped t;
-        t.header.stamp = this->get_clock()->now();
-        t.header.frame_id = "sun";
-        t.child_frame_id = get_name();
-        t.transform.translation.x = x;
-        t.transform.translation.y = y;
-        t.transform.translation.z = 0.0;
-        t.transform.rotation.w = 1.0;
+    geometry_msgs::msg::TransformStamped t;
+    t.header.stamp = this->get_clock()->now();
+    t.header.frame_id = "sun";
+    t.child_frame_id = get_name();
+    t.transform.translation.x = x;
+    t.transform.translation.y = y;
+    t.transform.translation.z = 0.0;
+    t.transform.rotation.w = 1.0;
 
-        // Broadcast the transform.
-        tf_broadcaster_->sendTransform(t);
+    // Broadcast the transform.
+    tf_broadcaster_->sendTransform(t);
 
-        // Update the marker.
-        marker_.header.stamp = this->get_clock()->now();
-        marker_.pose.position.x = x;
-        marker_.pose.position.y = y;
-        marker_.pose.position.z = 0.0;
-        marker_.pose.orientation.w = 1.0;
-        marker_.frame_locked = true;
+    // Update the marker.
+    marker_.header.stamp = this->get_clock()->now();
+    marker_.pose.position.x = x;
+    marker_.pose.position.y = y;
+    marker_.pose.position.z = 0.0;
+    marker_.pose.orientation.w = 1.0;
+    marker_.frame_locked = true;
 
-        vis_pub.publish(marker_);
-        
-    }
+    vis_pub.publish(marker_);
+    
+}
