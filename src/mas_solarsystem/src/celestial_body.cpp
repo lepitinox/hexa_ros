35a5a3c1be1a5f637d3c83db6c986ad2f7fea28f
Adrien Ducourthial <adrienducourthial@gmail.com>
2023-06-24 10:54:10 +0200

    qsdqsdqsd

--- src/mas_solarsystem/src/celestial_body.cpp
+++ src/mas_solarsystem/src/celestial_body.cpp
@@ -42,90 +42,90 @@ void CelestialBody::update()
 {   
     RCLCPP_INFO(this->get_logger(), "In update");
 
-}
-//    if (name == "Soleil"){
-//        geometry_msgs::msg::TransformStamped t;
-//        t.header.stamp = this->get_clock()->now();
-//        t.header.frame_id = "world";
-//        t.child_frame_id = name;
-//        t.transform.translation.x = 0.0;
-//        t.transform.translation.y = 0.0;
-//        t.transform.translation.z = 0.0;
-//        
-//    
-//        // Update the marker.
-//        visualization_msgs::msg::Marker marker;
-//        marker.header.stamp = this->get_clock()->now();
-//        marker.header.frame_id = name;
-//        marker.ns = "";
-//        marker.id = id;
-//        marker.type = visualization_msgs::msg::Marker::SPHERE;
-//        marker.action = visualization_msgs::msg::Marker::ADD;
-//        marker.pose.position.x = 1;
-//        marker.pose.position.y = 1;
-//        marker.pose.position.z = 1;
-//        marker.pose.orientation.x = 0.0;
-//        marker.pose.orientation.y = 0.0;
-//        marker.pose.orientation.z = 0.0;
-//        marker.pose.orientation.w = 1.0;
-//        marker.scale.x = 1;
-//        marker.scale.y = 10;
-//        marker.scale.z = 10;
-//        marker.color.a = 1.0; 
-//        marker.color.r = 1.0;
-//        marker.color.g = 1.0;
-//        marker.color.b = 1.0;
-////    marker_.frame_locked = true;
-//        marker_pub_->publish(marker);
-//        tf_broadcaster_->sendTransform(t);
-//
-//    }else{
-//
-//    auto g_constant_ = 6.67430e-11;
-//    double angular_velocity_ = calculate_omega(g_constant_, mass, orbit_radius);
-//
-//    double angle = angular_velocity_ * this->get_clock()->now().seconds();
-//
-//    double x = orbit_radius * cos(angle);
-//    double y = orbit_radius * sin(angle);
-//
-//    geometry_msgs::msg::TransformStamped t;
-//    t.header.stamp = this->get_clock()->now();
-//    t.header.frame_id = "Soleil";
-//    t.child_frame_id = name;
-//    t.transform.translation.x = x;
-//    t.transform.translation.y = y;
-//    t.transform.translation.z = 0.0;
-//    t.transform.rotation.w = 1.0;
-//
-//    // Broadcast the transform.
-//    
-//    
-//    // Update the marker.
-//    visualization_msgs::msg::Marker marker;
-//    marker.header.stamp = this->get_clock()->now();
-//    marker.header.frame_id = name;
-//    marker.ns = "";
-//    marker.id = id;
-//    marker.type = visualization_msgs::msg::Marker::SPHERE;
-//    marker.action = visualization_msgs::msg::Marker::ADD;
-//    marker.pose.position.x = 0.0;
-//    marker.pose.position.y = 0.0;
-//    marker.pose.position.z = 0.0;
-//    marker.pose.orientation.x = 0.0;
-//    marker.pose.orientation.y = 0.0;
-//    marker.pose.orientation.z = 0.0;
-//    marker.pose.orientation.w = 1.0;
-//    marker.scale.x = 10;
-//    marker.scale.y = 10;
-//    marker.scale.z = 10;
-//    marker.color.a = 1.0; 
-//    marker.color.r = 1.0;
-//    marker.color.g = 1.0;
-//    marker.color.b = 0.0;
-////    marker_.frame_locked = true;
-//    marker_pub_->publish(marker);
-//    tf_broadcaster_->sendTransform(t);
-//
-//    }
-// }
+
+    if (name == "Soleil"){
+        geometry_msgs::msg::TransformStamped t;
+        t.header.stamp = this->get_clock()->now();
+        t.header.frame_id = "world";
+        t.child_frame_id = name;
+        t.transform.translation.x = 0.0;
+        t.transform.translation.y = 0.0;
+        t.transform.translation.z = 0.0;
+        
+    
+        // Update the marker.
+        visualization_msgs::msg::Marker marker;
+        marker.header.stamp = this->get_clock()->now();
+        marker.header.frame_id = name;
+        marker.ns = "";
+        marker.id = id;
+        marker.type = visualization_msgs::msg::Marker::SPHERE;
+        marker.action = visualization_msgs::msg::Marker::ADD;
+        marker.pose.position.x = 1;
+        marker.pose.position.y = 1;
+        marker.pose.position.z = 1;
+        marker.pose.orientation.x = 0.0;
+        marker.pose.orientation.y = 0.0;
+        marker.pose.orientation.z = 0.0;
+        marker.pose.orientation.w = 1.0;
+        marker.scale.x = 1;
+        marker.scale.y = 10;
+        marker.scale.z = 10;
+        marker.color.a = 1.0; 
+        marker.color.r = 1.0;
+        marker.color.g = 1.0;
+        marker.color.b = 1.0;
+//    marker_.frame_locked = true;
+        marker_pub_->publish(marker);
+        tf_broadcaster_->sendTransform(t);
+
+    }else{
+
+    auto g_constant_ = 6.67430e-11;
+    double angular_velocity_ = calculate_omega(g_constant_, mass, orbit_radius);
+
+    double angle = angular_velocity_ * this->get_clock()->now().seconds();
+
+    double x = orbit_radius * cos(angle);
+    double y = orbit_radius * sin(angle);
+
+    geometry_msgs::msg::TransformStamped t;
+    t.header.stamp = this->get_clock()->now();
+    t.header.frame_id = "Soleil";
+    t.child_frame_id = name;
+    t.transform.translation.x = x;
+    t.transform.translation.y = y;
+    t.transform.translation.z = 0.0;
+    t.transform.rotation.w = 1.0;
+
+    // Broadcast the transform.
+    
+    
+    // Update the marker.
+    visualization_msgs::msg::Marker marker;
+    marker.header.stamp = this->get_clock()->now();
+    marker.header.frame_id = name;
+    marker.ns = "";
+    marker.id = id;
+    marker.type = visualization_msgs::msg::Marker::SPHERE;
+    marker.action = visualization_msgs::msg::Marker::ADD;
+    marker.pose.position.x = 0.0;
+    marker.pose.position.y = 0.0;
+    marker.pose.position.z = 0.0;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
+    marker.scale.x = 10;
+    marker.scale.y = 10;
+    marker.scale.z = 10;
+    marker.color.a = 1.0; 
+    marker.color.r = 1.0;
+    marker.color.g = 1.0;
+    marker.color.b = 0.0;
+//    marker_.frame_locked = true;
+    marker_pub_->publish(marker);
+    tf_broadcaster_->sendTransform(t);
+
+    }
+ }
